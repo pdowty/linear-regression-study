20e8fbf6ff779ffd179e7310b9229ecb1494c5ad
pdowty <peter.dowty@dnr.wa.gov>
2026-02-16 21:18:48 -0800

    Building residual diagram code.

--- code/residual_diagram_creation.r
+++ code/residual_diagram_creation.r
@@ -14,7 +14,7 @@ library(tidyverse)
 
 
 ###############################################################################
-# set simulation parameters
+# set simulation parameters and initialize
 ###############################################################################
 Xmin <- 0
 Xmax <- 100
@@ -23,6 +23,11 @@ m <- 0.25
 b <- 0
 N <- 10  # number of data points available for regression
 Niterations <- 1000   # number of iterations of data collection
+
+# initialize data frame to hold iteration linear regression results
+regr_iterations <- data.frame(intcpt=numeric(), se_intcpt=numeric(),
+                              p_intcpt=numeric(), slope=numeric(),
+                              se_slope=numeric(), p_slope=numeric())
 
 
 ###############################################################################
@@ -52,12 +57,32 @@ y_df <- m * x_df + b + residuals
 
 
 ###############################################################################
-# Get linear regression lines and stats for all iterations of input data 
+# Loop through iterations - assemble data, get regression, store results 
 ###############################################################################
-
-
-
-
-
+for (i in seq(1,Niterations)) {
+  # print progress to console periodically
+  if (i %% 100 == 0) {
+    print(sprintf("iteration %d",i))
+  }
+  # assemble data
+  iter_data <- data.frame(x = x_values, y = as.vector(t(y_df[i,])))
+  names(iter_data) <- c("x", "y")
+  # perform regression
+  lm_obj <- lm(y ~ x, data = iter_data)
+  # access & store results
+  model_summary <- summary(lm_obj)
+  coeff_matrix <- model_summary$coefficients
+  iter_results <- data.frame(intcpt = coeff_matrix[1,1],
+                             se_intcpt = coeff_matrix[1,2],
+                             p_intcpt = coeff_matrix[1,4],
+                             slope = coeff_matrix[2,1],
+                             se_slope = coeff_matrix[2,2],
+                             p_slope = coeff_matrix[2,4])
+  regr_iterations <- rbind(regr_iterations, iter_results) 
+  
+  # NEED TO RETRIEVE AND STORE RESIDUALS
+  # in vector lm_obj$residuals
+  
+}
 
 
